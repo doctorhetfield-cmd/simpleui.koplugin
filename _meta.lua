ad474a02c6c36b34333dc8b1b8fd23b2a5c13618
gswang <46040546+GOSENGWONG@users.noreply.github.com>
2026-07-11 00:08:05 +0800

    Improve translation support
    Add translation support for "fullname" and "description", and complete the translation of plugin metadata displayed in KOReader's plugin manager.

--- _meta.lua
+++ _meta.lua
@@ -1,9 +1,10 @@
-
+local I18n = require("sui_i18n")
+local _    = I18n.translate
 
 return {
     name        = "simpleui",
-    fullname    = "Simple UI",
-    description = [[A simple UI for KOReader]],
+    fullname    = _("Simple UI"),
+    description = _([[A simple UI for KOReader]]),
     version     = "2.1",
     author      = "Doctor Hetfield",
 }
